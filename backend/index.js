e067eaef743f31b0845add9c1927bd7f2fc86a82
dzabbah <sergerufin1984@gmail.com>
2025-04-09 03:23:39 +0000

    backend du projet fini avec ses quelques erreur sur le postman qui affiche des erreurs

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./src/lib/db.js";
+
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(5001, () => {
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+});
